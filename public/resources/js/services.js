aae0e4d0396ae118f9e4a2dee04a8703fb913d01
Kenneth Ah Young <kenneth@syple.com.au>
2013-09-18 22:06:47 +1000

    Asking for all permissions from user

--- public/resources/js/services.js
+++ public/resources/js/services.js
@@ -13,7 +13,7 @@ angular.module('WanderApp.services', []).
     //Store information about the application here
     var appId = "568077023249265";
   	var appSecret = "86a2efda2032238a960e70fe3e825bdf";
-    var scope = {'scope': 'email,user_about_me,user_activities,user_birthday,user_checkins'};
+    var scope = {'scope': 'user_about_me,user_activities,user_birthday,user_checkins,user_education_history,user_events,user_groups,user_hometown,user_interests,user_likes,user_location,user_notes,user_photos,user_questions,user_relationships,user_relationship_details,user_religion_politics,user_status,user_subscriptions,user_videos,user_website,user_work_history,friends_about_me,friends_activities,friends_birthday,friends_checkins,friends_education_history,friends_events,friends_groups,friends_hometown,friends_interests,friends_likes,friends_location,friends_notes,friends_photos,friends_questions,friends_relationships,friends_relationship_details,friends_religion_politics,friends_status,friends_subscriptions,friends_videos,friends_website,friends_work_history'};
 
     // Throw error if FB does not exist (not yet instantiated)
     if(!FB) throw new Error('Facebook not loaded');
